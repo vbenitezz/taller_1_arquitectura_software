da724dca62cf0a273d8082c713f86b4c0b6f59a3
Darieth <darieth.s.v@gmail.com>
2024-09-03 15:18:46 -0500

    Customer view implementation

--- sales_module/views.py
+++ sales_module/views.py
@@ -4,5 +4,5 @@ from .models import Published_Product
 # Create your views here.
 
 def home(request):
-    # products = Published_Product.objects.filter(publish_type="sale")
-    return render(request, 'template_sales_module.html')
+    products = Published_Product.objects.filter(publish_type="sale")
+    return render(request, 'template_sales_module.html',{'products':products})
