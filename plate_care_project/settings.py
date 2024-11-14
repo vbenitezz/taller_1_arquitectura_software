d23f2e480b647d2e38718aa7b28bff3590cc1f08
Darieth <darieth.s.v@gmail.com>
2024-11-14 09:15:58 -0500

    project deployment

--- plate_care_project/settings.py
+++ plate_care_project/settings.py
@@ -45,10 +45,10 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'http://35.222.205.184'
+    'http://34.56.224.38'
 ]
 
-ALLOWED_HOSTS=['35.222.205.184']
+ALLOWED_HOSTS=['34.56.224.38']
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
